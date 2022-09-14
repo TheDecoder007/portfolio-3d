2589d89962248adb38f93447c37f433ae98de0c0
Ryan Murphy <TheDecoder007@gmail.com>
2022-09-14 14:25:11 -0400

    style changes, camera changes

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import './style.css'
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { Vector3, WebGLArrayRenderTarget } from 'three';
+import { Vector3 } from 'three';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGL1Renderer({
   canvas: document.querySelector('#bg'),
@@ -15,18 +15,31 @@ const renderer = new THREE.WebGL1Renderer({
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-camera.position.setZ(30);
-const murphTexture = new THREE.TextureLoader().load('MurpheusFace.jpeg')
+camera.position.setZ(2);
+camera.position.setX(0)
+camera.position.setY(1)
 
-const geometry  = new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshStandardMaterial( { map: murphTexture } );
+
+const murphTexture = new THREE.TextureLoader().load('MurpheusFace.jpeg')
+const spaceTexture = new THREE.TextureLoader().load('CloudsGoodDark.jpg');
+const spaceTexture2 = new THREE.TextureLoader().load('CloudsGood.jpg');
+
+scene.background = spaceTexture;
+
+
+const geometry  = new THREE.TorusGeometry( 8, 1, 6, 5 )
+const material = new THREE.MeshStandardMaterial( { map: spaceTexture2 } );
 const torus = new THREE.Mesh( geometry, material );
 scene.add(torus)
 
 const loader = new GLTFLoader();
 loader.load( 'Murph_Sparkles.glb', function (gltf)  {
   scene.add(gltf.scene);
-  // gtlf.scene.scale: Vector3(4,4,4);
+  gltf.scene.position.y = -1.2;
+
+  gtlf.scene.scale(4, 4, 4);
+ 
+
 });
 
 const pointLight2 = new THREE.PointLight(0xffffff)
@@ -38,28 +51,39 @@ pointLight.position.set(5, 5, 5,)
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, pointLight2, ambientLight)
 
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-// const lightHelper2 = new THREE.PointLightHelper(pointLight2)
 
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, lightHelper2, gridHelper)
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const lightHelper2 = new THREE.PointLightHelper(pointLight2)
+const cameraHelper = new THREE.CameraHelper(camera);
+const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, lightHelper2, gridHelper, cameraHelper)
+
  
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
 const murph = new THREE.Mesh(
-  new THREE.BoxGeometry(3,3.5,3),
+  new THREE.BoxGeometry(4,5,4),
   new THREE.MeshBasicMaterial( { map: murphTexture } )
 );
 
 scene.add(murph);
-murph.position.z = -30;
-murph.position.setX(-5);
+murph.position.z = 3;
+murph.position.setX(-20);
+
+const murph2 = new THREE.Mesh(
+  new THREE.BoxGeometry(4,5,4),
+  new THREE.MeshBasicMaterial( { map: murphTexture } )
+);
+
+scene.add(murph2);
+murph2.position.z = 3;
+murph2.position.setX(20);
 
 
 
 function addStar() {
- const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+ const geometry = new THREE.SphereGeometry(0.1, 24, 24);
  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } )
  const star = new THREE.Mesh( geometry, material );
 
@@ -69,19 +93,18 @@ function addStar() {
  scene.add(star)
 }
 
-Array(200).fill().forEach(addStar)
+Array(300).fill().forEach(addStar)
 
-const spaceTexture = new THREE.TextureLoader().load('CloudsGood.jpg');
-scene.background = spaceTexture;
 
 function moveCamera() {
 
   const t = document.body.getBoundingClientRect().top;
-  murph.rotation.y += 0.05;
+  murph.rotation.y += 0.09;
+  murph2.rotation.y += -0.09;
 
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.position.y = t * -0.0002;
+  camera.position.z = t * -0.012;
+  camera.position.x = t * -0.000002;
+  camera.position.y = t * -0.006;
 }
 
 document.body.onscroll = moveCamera
@@ -89,9 +112,9 @@ document.body.onscroll = moveCamera
 function animate() {
     requestAnimationFrame( animate );
 
-torus.rotation.x += 0.01;
-torus.rotation.y += 0.005;
-torus.rotation.z += 0.01;
+torus.rotation.x += 0.004;
+torus.rotation.y += 0.003
+torus.rotation.z += 0.02;
 
 controls.update();
 
